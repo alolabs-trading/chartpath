62f09c4329669acf93d3624c03380c5e5b60f60d
alolabs-trading <alolabs.in@gmail.com>
2025-09-15 17:07:59 +0530

    Fix indentation in serve_image function

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/chart/<path:filename>')
 def serve_image(filename):
-   return send_from_directory('/chart', filename)
+    return send_from_directory('/chart', filename)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
